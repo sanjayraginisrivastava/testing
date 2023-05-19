6994a7b26b17373a6dc6c9d300f08478a0a9d7b2
Sanjay <sanjayraginisrivastava60@gmail.com>
2023-05-20 00:45:21 +0530

    first commit

---
+++ keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoUrl:"mongodb+srv://sanjayraginisrivastava60:*ss0208ss*@cluster0.6xxx7n7.mongodb.net/?retryWrites=true&w=majority",
+    Jwt_secret : "12345"
+    
+
+
+};
