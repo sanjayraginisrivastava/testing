e04355460be0e2cd1753ba18fb393c713d8813a7
sanjayraginisrivastava <134097155+sanjayraginisrivastava@users.noreply.github.com>
2023-05-27 02:10:39 +0530

    second commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "./shivareels1/build")))
 
 app.get("*",(req,res)=>{
     res.sendFile(
-        path.join(__dirname, "./shivareels/build/index.html"),
+        path.join(__dirname, "./shivareels1/build/index.html"),
         function(err) {
             res.status(500).send(err)
         }
